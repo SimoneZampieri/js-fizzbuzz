002a76d6f6e26a23d91a3976708a3e866aded5c5
SimoneZampieri <simonezampieri106@gmail.com>
2024-11-14 13:51:15 +0100

    [end] esercizio completo, readme corretto

--- JS/script.js
+++ JS/script.js
@@ -6,3 +6,23 @@
 
 console.log('===== stampo i numeri da 1 a 100 =====');
 
+for (let i=1; i <= 100; i++){ //ora definiamo le istruzioni
+
+    if ( i % 15 == 0 ){
+        console.log('FizzBuzz');  //15 ed i numeri condivisi fra i multipli di 5 e 3 avranno denominazione FizzBuzz
+    }
+        
+    else if ( i % 3 == 0){
+        console.log('Fizz') //3 ed i suoi multipli mostrereanno la denominazione Fizz
+    }
+
+    else if ( i % 5 == 0){
+        console.log('Buzz') //5 ed i suoi multipli mostreranno la denominazione Buzz
+    }
+
+    else{
+        console.log(i) //gli altri numeri, non essendo multipli di 3 o 5, saranno invariati
+    }
+       
+}
+   
--- README.md
+++ README.md
@@ -8,5 +8,6 @@ FizzBuzz
 - Rendiamo noto un ciclo di numeri da 1 a 100, in questo caso utilizzando for (i=1; i < 100; i++)
 - A questo punto, avremo tutti i numeri disponibili, e possiamo definire la condizione if (i % 3) = Fizz
 - Per i multipli di 5 invece: else (i % 5) = Buzz 
-- Nel caso del numero 15, dovrà mostrare entrambi i valori
+- Nel caso del numero 15 o degli altri numeri condivisi fra i due multipli, dovranno mostrare entrambi i valori
+- Per tutti gli altri numeri non cambierà nulla, indi per cui si mostreranno come console.log(i)
 
